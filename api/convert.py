0891140f26627c48c76befc8f9869f970b6ac77f
Srijayan Pallerla <srijayanc5@gmail.com>
2026-06-05 01:30:41 -0500

    restructure for vercel deployment

--- api/convert.py
+++ api/convert.py
@@ -1,16 +1,14 @@
-from flask import Flask, request, jsonify
+from flask import request, jsonify
 from PIL import Image
 from flask_cors import CORS
 import io
-
-app = Flask(__name__)
-CORS(app)
 
 ASCII_CHARS = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'. "
 
 def image_to_ascii(image_bytes, width=100, invert=False):
     chars = ASCII_CHARS[::-1] if invert else ASCII_CHARS
     img = Image.open(io.BytesIO(image_bytes)).convert("L")
+    img.thumbnail((1200, 1200))
     original_width, original_height = img.size
     height = int(original_height * width / original_width * 0.45)
     img = img.resize((width, height))
@@ -22,9 +20,8 @@ def image_to_ascii(image_bytes, width=100, invert=False):
             ascii_str += "\n"
     return ascii_str
 
-@app.route("/convert", methods=["POST"])
-def convert():
-    try:
+def handler(request):
+    if request.method == "POST":
         file = request.files.get("image")
         if not file:
             return jsonify({"error": "No image uploaded"}), 400
@@ -32,9 +29,3 @@ def convert():
         invert = request.form.get("invert") == "true"
         result = image_to_ascii(file.read(), width, invert)
         return jsonify({"ascii_art": result})
-    except Exception as e:
-        print(" Error:", e)
-        return jsonify({"error": "Failed to convert image"}), 500
-
-if __name__ == "__main__":
-    app.run(debug=True, port = 5001)
